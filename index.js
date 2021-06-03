9f7e89134f693f91141b20ae5a6b7ca60533a69b
elvis-duoc <81184929+elvis-duoc@users.noreply.github.com>
2021-06-02 23:45:03 -0400

    Actualizacion error footer

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ const setWeatherData = data => {
 
 const cleanUp = () => {
     let container = document.getElementById('container');
+    let loader = document.getElementById('loader');
+
+    loader.style.display = 'none'; 
     container.style.display = 'flex'; 
 }
 
