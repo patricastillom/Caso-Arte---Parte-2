e1733ac5f1bb683756afbda001f37541c0c48984
elvis-duoc <81184929+elvis-duoc@users.noreply.github.com>
2021-06-02 23:30:24 -0400

    Integración de API del Clima

---
+++ index.js
@@ -0,0 +1,41 @@
+const APP_ID = '4090239d69cdb3874de692fd18539299';
+
+const fetchData = position => {
+    const { latitude, longitude } = position.coords;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${APP_ID}`)
+        .then(response => response.json())
+        .then(data => setWeatherData(data));
+}
+
+const setWeatherData = data => {
+    const weatherData = {
+
+        //icon: "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png",
+        location: "COMUNA: "+ data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: Math.floor(data.main.temp),
+
+    }
+    
+    Object.keys(weatherData).forEach( key => {
+    setTextContent(key, weatherData[key]);
+    });
+
+    cleanUp();
+}
+
+const cleanUp = () => {
+    let container = document.getElementById('container');
+    container.style.display = 'flex'; 
+}
+
+
+const setTextContent = (element, text) => {
+    document.getElementById(element).textContent = text;
+}
+
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData)
+}
